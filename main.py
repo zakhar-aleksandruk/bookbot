c4f394328f5d5da39743ef7130e19b26c1767da1
zakhar-aleksandruk <zakhar.aleksandruk@gmail.com>
2024-07-29 10:59:45 +0300

    Add character counting ability

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ def main():
     path = "./books/frankenstein.txt"
     book = get_book_text(path)
     print(count_words(book))
+    print(count_characters(book))
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(str):
-    words = str.split()
+def count_words(text):
+    words = text.split()
     
     return len(words)
+
+def count_characters(text):
+    chars = {}
+
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+            continue
+
+        chars[char] = 1
+    
+    return chars
 
 main()
